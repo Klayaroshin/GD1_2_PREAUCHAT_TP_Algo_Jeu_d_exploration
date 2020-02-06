6d15de0729fbdde4e26353426df6a27ba5e2d3a3
Klayaroshin <jerome.preauchat@gmail.com>
2020-02-06 16:51:17 +0100

    Debut exo 4

--- explo.c
+++ explo.c
@@ -24,8 +24,9 @@ int deplacement(lieu lieu1, lieu lieu2, lieu lieu3) {
 		if (strcmp(ChoixDestination,"Heilheim")== 0) {
 			printf("%s",lieu1.description);
 			printf ( "vous etes arrive a Heilheim" );
-			printf("Voulez vous maintenant vous diriger vers l'Olympe(1) ou Asgard(2) ?");
+			printf("Voulez vous maintenant vous diriger vers les cieux (1) ou Asgard(2) ?");
 			scanf("%d",lieu1.id);
+			// if (lieu1.id = {
 		}
 		 
 		if (strcmp(ChoixDestination,"Tristram")== 0) {
@@ -46,11 +47,20 @@ int deplacement(lieu lieu1, lieu lieu2, lieu lieu3) {
 int main(){
 	
 	lieu lieu1 ={"Heilheim", "C'est un lieu brumeux \n", 10, {1,2}};
-	lieu lieu2 ={"Tristram", "C'est un lieu sombre \n", 11, {1,2}};
-	lieu lieu3 ={"Olympe", "C'est un lieu tres haut perche\n", 18, {1,2}};
-	//lieu lieu4 ={"Asgard", "C'est un lieu tres lumineux\n", 15, {1,2}};
-	//lieu lieu5 ={"Avalon", "C'est un lieu plein d'aventuriers\n", 6, {1,2}};
-	//lieu lieu6 ={"Valhalla", "C'est un lieu plein d'ames perdues\n", 12, {1,2}};
+	lieu lieu2 ={"Tristram", "C'est un lieu sombre \n", 11, {2,3}};
+	lieu lieu3 ={"Olympe", "C'est un lieu tres haut perche\n", 18, {3,4}};
+	
+	lieu lieux[10] = {"Heilheim", "C'est un lieu brumeux \n", 10, {1,2},
+					 "Tristram", "C'est un lieu sombre \n", 11, {2,3},
+					 "Cieux", "C'est un tres tres lumineux \n", 7, {1,3},
+					 "Olympe", "C'est un lieu tres haut perche\n", 18, {3,4},
+					 "Asgard", "C'est un lieu tres lumineux\n", 15, {1,4},
+					 "Avalon", "C'est un lieu plein d'aventuriers\n", 6, {2,4},
+					 "Valhalla","C'est un lieu plein d'ames perdues\n", 12, {2,5},
+					 "Midgard","C'est un lieu avec des nains\n", 12, {3,5},
+					 "Eldorado","C'est un lieu plein de ressources\n", 12, {3,6}
+					 };
+					 
 	deplacement(lieu1, lieu2, lieu3);
 	
 	
